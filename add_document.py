b1b1819479746878cf151178e8a052ce9ed21c3e
박용주 <apexacme@gmail.com>
2024-07-07 12:28:26 +0900

    Create add_document.py

---
+++ add_document.py
@@ -0,0 +1,37 @@
+import logging
+import os
+import sys
+
+from dotenv import load_dotenv
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import UnstructuredPDFLoader
+from langchain_community.vectorstores import Pinecone
+from langchain_openai import OpenAIEmbeddings
+
+load_dotenv()
+
+logging.basicConfig(
+    format="%(asctime)s [%(levelname)s] %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+
+def initialize_vectorstore():
+    index_name = os.environ["PINECONE_INDEX"]
+    embeddings = OpenAIEmbeddings()
+    return Pinecone.from_existing_index(index_name, embeddings)
+
+
+if __name__ == "__main__":
+    # file_path = sys.argv[1]
+    file_path = "./클라우드 네이티브 정보시스템 구축을 위한 발주자 안내서.pdf"
+    loader = UnstructuredPDFLoader(file_path)
+    raw_docs = loader.load()
+    logger.info("Loaded %d documents", len(raw_docs))
+
+    text_splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=30)
+    docs = text_splitter.split_documents(raw_docs)
+    logger.info("Split %d documents", len(docs))
+
+    vectorstore = initialize_vectorstore()
+    vectorstore.add_documents(docs)
